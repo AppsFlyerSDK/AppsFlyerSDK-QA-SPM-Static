287ae9c9aab219a8c23b827443a8eac01f1e84a6
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2024-03-25 18:32:51 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AppsFlyerLib",
+    products: [
+        .library(
+            name: "AppsFlyerLib-Static",
+            targets: ["AppsFlyerLib"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppsFlyerLib",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.13.1/AppsFlyerLib.xcframework.zip",
+            checksum: "6ffb31e9b561670afe209a5357db6d4bddc54d21ef0d3ebfdbd5476509c5ffb4"
+        )
+    ]
+)
